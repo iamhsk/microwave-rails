6558ea4e722880a9eb5959e7cc4e0e90d6beac2e
Valerie Roske <vroske@gmail.com>
2015-06-10 10:49:20 -0500

    #7: allow idp to hit saml callback

--- rails-app/app/controllers/sessions_controller.rb
+++ rails-app/app/controllers/sessions_controller.rb
@@ -1,7 +1,10 @@
 class SessionsController < ApplicationController
 
+  skip_before_action :verify_authenticity_token, only: [:callback]
+
   def callback
     session[:auth_info] = request.env["omniauth.auth"]
+    puts session[:auth_info]
     redirect_to root_url
   end
 
