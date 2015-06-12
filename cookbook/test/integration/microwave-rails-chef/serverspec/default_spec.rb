024174233d0774ecb7493177750d53ff616224de
Valerie Roske <vroske@gmail.com>
2015-06-12 11:11:10 -0500

    some quick fixes

--- cookbook/test/integration/microwave-rails-chef/serverspec/default_spec.rb
+++ cookbook/test/integration/microwave-rails-chef/serverspec/default_spec.rb
@@ -1,4 +1,4 @@
-require 'serverspec'
+require "serverspec"
 
 USER = "vagrant"
 PATH = "/opt/rbenv/bin:/opt/rbenv/shims:$PATH";
@@ -13,34 +13,34 @@ RSpec.configure do |c|
 end
 
 # Ruby 2.2.2
-describe package('ruby') do
+describe package("ruby") do
   it { should be_installed}
 end
 
-describe command('ruby -v') do
-  its(:stdout) { should match '2.2.2' }
+describe command("ruby -v") do
+  its(:stdout) { should match "2.2.2" }
 end
 
 # Bundle
-describe command('which bundle') do
-  its(:stdout) { should match '/opt/rbenv/shims/bundle' }
+describe command("which bundle") do
+  its(:stdout) { should match "/opt/rbenv/shims/bundle" }
 end
 
-describe command('cd #{MICROWAVE_HOME} && bundle install') do
+describe command("cd #{MICROWAVE_HOME} && bundle install") do
   its(:exit_status) { should eq 0 }
 end
 
 # Postgresql
-describe service('postgresql') do
+describe service("postgresql") do
   it { should be_running }
 end
 
-describe command('cd #{MICROWAVE_HOME} && bundle exec rake db:create db:migrate') do
+describe command("cd #{MICROWAVE_HOME} && bundle exec rake db:drop db:create db:migrate db:seed") do
   its(:exit_status) { should eq 0 }
 end
 
 # Node.js (required by Rails 3 asset pipeline)
-describe package('nodejs') do
+describe package("nodejs") do
   it { should be_installed }
 end
 
